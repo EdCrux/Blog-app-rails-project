60ed4561bae4eec3bb616e4254166c632f91b8fa
Eduardo <jecruxiz@gmail.com>
2020-04-21 17:54:12 -0500

    :sparkles: Introducing the new , create controller with their respective view
    Also implementing a method that permits only the information that we need from the form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,27 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id]); 
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(white_list)
+    if @article.save
+      redirect_to :articles
+    else
+      render 'new'
+    end
+  end
+
+  def white_list
+    params.require(:article).permit(:title,:body)
+  end
+
+
 end
